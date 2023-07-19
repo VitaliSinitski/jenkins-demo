892ffc719d821cd1fe78883ba8a0e4c1332bba5d
Vitali Sinitski <vitali.sin777@gmail.com>
2023-07-19 03:18:16 +0300

    1.0

---
+++ src/main/java/com/example/HelloController.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping
+    public String helloWorld() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/{name}")
+    public String helloName(@PathVariable String name) {
+        return "Hello " + name + "!";
+    }
+}
+
