a2414fabbd2de67d303e7b18f81b7066e3bf8220
Vitali Sinitski <vitali.sin777@gmail.com>
2023-07-20 01:53:45 +0300

    1.5

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -11,7 +11,7 @@ public class HelloController {
 
     @GetMapping
     public String helloWorld() {
-        return "Go to hell, World!";
+        return "Hello World!";
     }
 
     @GetMapping("/{name}")
